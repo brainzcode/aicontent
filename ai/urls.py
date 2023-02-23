5226f56d1b658f1192dbfefbc5cbd0e533bccddf
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-02-23 15:15:45 +0100

    about.html updated

--- ai/urls.py
+++ ai/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, about
 
 urlpatterns = [
     path('', home, name='home-page'),
+    path('about-ai-content', about, name='about-page'),
 ]
--- ai/views.py
+++ ai/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'ai/index.html', {})
+
+
+def about(request):
+    return render(request, 'ai/about.html', {})

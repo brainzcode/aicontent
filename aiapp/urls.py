231029c8cc85eea4a396f0ccffcb79d4802e6316
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-02-23 14:11:48 +0100

    landing updated

--- aiapp/urls.py
+++ aiapp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import settings
 
 urlpatterns = [
@@ -24,5 +24,4 @@ urlpatterns = [
     path('', include('ai.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += staticfiles_urlpatterns()
-
+# urlpatterns += staticfiles_urlpatterns()

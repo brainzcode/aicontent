7a6b834373f332ea1acfdb4ffa310bd09fcbde9e
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-03-08 21:23:51 +0100

    stacticfiles url updated

--- aiapp/settings.py
+++ aiapp/settings.py
@@ -138,11 +138,11 @@ CORS_ALLOWED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')

b12046ab5d4b9d620cf061e7ac68a2b6a55a04d8
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-03-22 15:17:54 +0100

    profile url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
     path('home', home, name='dashboard'),
+    path('profile', profile, name='profile'),
 ]
